e4d6e573b207d22f1203c37ad68adca23c6cd8b8
nancybana <nancybana404@gmail.com>
2026-08-19 09:10:56 +0530

    DAY-10

---
+++ DAY-10.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for(int i = 1; i < nums.size(); i++) {
+            currentSum = max(nums[i], currentSum + nums[i]);
+            maxSum = max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+};
+
+int main() {
+    vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+    Solution obj;
+    cout << "Maximum Subarray Sum: "
+         << obj.maxSubArray(nums);
+
+    return 0;
+}
