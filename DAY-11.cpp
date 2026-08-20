1b130bb6dbd8fdf01670b64fe0af57bdaa2a4fa9
nancybana <nancybana404@gmail.com>
2026-08-20 11:14:00 +0530

    DAY-11

---
+++ DAY-11.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void moveZeroes(vector<int>& nums) {
+    int j = 0; // Position for next non-zero element
+
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] != 0) {
+            swap(nums[i], nums[j]);
+            j++;
+        }
+    }
+}
+
+int main() {
+    vector<int> nums = {0, 1, 0, 3, 12};
+
+    moveZeroes(nums);
+
+    for (int num : nums) {
+        cout << num << " ";
+    }
+
+    return 0;
+}
