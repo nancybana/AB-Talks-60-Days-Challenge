cbf03195f9915dab85bfd7f27c0bbffd72a768e8
nancybana <nancybana404@gmail.com>
2026-08-25 20:04:44 +0530

    DAY-15

---
+++ DAY-15.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool containsDuplicateBrute(vector<int>& nums)
+{
+    int n = nums.size();
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = i + 1; j < n; j++)
+        {
+            if(nums[i] == nums[j])
+            {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+int main()
+{
+    vector<int> nums = {1, 2, 3, 1};
+
+    cout << containsDuplicateBrute(nums);
+
+    return 0;
+}
