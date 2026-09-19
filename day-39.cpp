7974132e90487a29441a110dfbfa522c96902fac
nancybana <nancybana404@gmail.com>
2026-09-19 22:43:35 +0530

    DAY-39

---
+++ day-39.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) {
+        val = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int maxDepth(TreeNode* root) {
+    if (root == NULL)
+        return 0;
+
+    return 1 + max(maxDepth(root->left), maxDepth(root->right));
+}
+
+int main() {
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
+
+    cout << "Maximum Depth = " << maxDepth(root);
+
+    return 0;
+}
