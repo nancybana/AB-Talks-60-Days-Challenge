dbd823b86251a8fed13ed1d63732def3c4425999
nancybana <nancybana404@gmail.com>
2026-09-20 21:29:53 +0530

    DAY-40

---
+++ day-40.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+class TreeNode {
+public:
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) {
+        val = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class Solution {
+public:
+    bool check(TreeNode* root, long long minVal, long long maxVal) {
+        if (root == NULL)
+            return true;
+
+        if (root->val <= minVal || root->val >= maxVal)
+            return false;
+
+        return check(root->left, minVal, root->val) &&
+               check(root->right, root->val, maxVal);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        return check(root, LLONG_MIN, LLONG_MAX);
+    }
+};
+
+int main() {
+    TreeNode* root = new TreeNode(5);
+    root->left = new TreeNode(3);
+    root->right = new TreeNode(7);
+    root->left->left = new TreeNode(2);
+    root->left->right = new TreeNode(4);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(8);
+
+    Solution obj;
+
+    if (obj.isValidBST(root))
+        cout << "Valid BST";
+    else
+        cout << "Invalid BST";
+
+    return 0;
+}
